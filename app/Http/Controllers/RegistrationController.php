6260eb5d27104a30f2001f0801847abc97317ab2
Ahmed Sukhera <ahmed.sukhera@gmail.com>
2019-02-05 06:11:31 -0500

    Registration code update

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Meeting;
+use App\User;
+use JWTAuth;
 
 class RegistrationController extends Controller
 {
@@ -20,7 +23,43 @@ class RegistrationController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'meeting_id' => 'required',
+            'user_id' => 'required',
+        ]);
 
+        $meeting_id = $request->input('meeting_id');
+        $user_id = $request->input('user_id');
+
+        $meeting = Meeting::findOrFail($meeting_id);
+        $user = User::findOrFail($user_id);
+
+        $message = [
+            'msg' => 'User is already registered from meeting',
+            'user' => $user,
+            'meeting' => $meeting,
+            'unregister' => [
+                'href' => 'api/v1/meeting/registration/' . $meeting->id,
+                'method' => 'DELETE',
+            ]
+        ];
+        if ($meeting->users()->where('users.id', $user->id)->first()) {
+            return response()->json($message, 404);
+        };
+
+        $user->meetings()->attach($meeting);
+
+        $response = [
+            'msg' => 'User registered for meeting',
+            'meeting' => $meeting,
+            'user' => $user,
+            'unregister' => [
+                'href' => 'api/v1/meeting/registration/' . $meeting->id,
+                'method' => 'DELETE'
+            ]
+        ];
+
+        return response()->json($response, 201);
     }
 
 
@@ -34,6 +73,27 @@ class RegistrationController extends Controller
     public function destroy($id)
     {
         //
-        return "It works";
+        $meeting = Meeting::findOrFail($id);
+        if (! $user = JWTAuth::parseToken()->authenticate()) {
+            return response()->json(['msg' => 'User not found'], 404);
+        }
+        if (!$meeting->users()->where('users.id', $user->id)->first()) {
+            return response()->json(['msg' => 'user not registered for meeting, delete operation not successful'], 401);
+        };
+
+        $meeting->users()->detach($user->id);
+
+        $response = [
+            'msg' => 'User unregistered for meeting',
+            'meeting' => $meeting,
+            'user' => $user,
+            'register' => [
+                'href' => 'api/v1/meeting/registration',
+                'method' => 'POST',
+                'params' => 'user_id, meeting_id'
+            ]
+        ];
+
+        return response()->json($response, 200);
     }
 }
